eca53e412a5ff50ac1b9e21d56d8bbfd3d738bea
Osiris <tim@chaubet.be>
2017-07-26 17:44:34 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+set -e
+
+### variables
+initfile=musicbrainz.initialised
+[[ -z "${PGPORT// }" ]] && port=5432 || port=$PGPORT
+[[ -z "${PGHOST// }" ]] && dbhost="musicbrainz-database" || dbhost=$PGHOST
+
+### functions
 run_sql_file() {
-   if [ -n $PGHOST ]; then
-      if [ -n $PGPORT ]; then
-         port=$PGPORT
-      else 
-         port=5432
-      fi
-      echo "executing \"psql -h $PGHOST -p $port -d musicbrainz -U $PGUSER -a -f $1\""
-      psql -h $PGHOST -p $port -d musicbrainz -U $PGUSER -a -f $1
+   if [ ! -z "{$PGHOST// }" ]; then
+      echo "executing \"psql -h $dbhost -p $port -d musicbrainz -U $PGUSER -a -f $1\""
+      psql -h $dbhost -p $port -d musicbrainz -U $PGUSER -a -f $1
    else
       echo "executing \"psql -d musicbrainz -U $PGUSER -a -f $1\""
       psql -d musicbrainz -U $PGUSER -a -f $1
    fi
 }
-set -e
-initfile=musicbrainz.initialised
+run_sql_query() {
+   if [ ! -z "{$PGHOST// }" ]; then
+      psql -h $dbhost -p $port -d musicbrainz -U $PGUSER -$1 -c "$2"
+   else
+      psql -d musicbrainz -U $PGUSER -$1 -c "$2"
+   fi
+}
+
+
+
 if [ ! -d /www ]; then
    mkdir -p /www
    echo "<? header('Location: /test.php'); ?>" > /www/index.php
@@ -23,7 +34,7 @@ if [ ! -d /www ]; then
 fi   
 if [ ! -e ~/.pgpass ]; then
    echo creating ~/.pgpass
-   echo "$PGHOST:$PGPORT:musicbrainz:$PGUSER:$PGPASS"  > ~/.pgpass
+   echo "$dbhost:$port:musicbrainz:$PGUSER:$PGPASS"  > ~/.pgpass
    chmod 0600 ~/.pgpass
 fi
 if [ ! -d /www/sqls ]; then
@@ -74,10 +85,11 @@ if [ ! -d /www/dump/mbdump ]; then
    echo "Uncompressing Musicbrainz mbdump.tar.bz2"
    tar xjf /www/dump/mbdump.tar.bz2 -C /www/dump/mbdump
 fi
-#psql -h $PGHOST -p $PGPORT -d musicbrainz -U $PGUSER -l
-if [ $(psql -h $PGHOST -p $PGPORT -d musicbrainz -U $PGUSER -t -c "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'musicbrainz');") == "f" ]; then
+#if [ $(psql -h $PGHOST -p $PGPORT -d musicbrainz -U $PGUSER -t -c "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'musicbrainz');") == "f" ]; then
+if [ $(run_sql_query "t" "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = 'musicbrainz');") ]; then
    echo "creating database schema musicbrainz"
-   psql -h $PGHOST -p $PGPORT -d musicbrainz -U $PGUSER -a -c "CREATE SCHEMA musicbrainz"
+   run_sql_query "a" "CREATE SCHEMA musicbrainz"
+   #psql -h $PGHOST -p $PGPORT -d musicbrainz -U $PGUSER -a -c "CREATE SCHEMA musicbrainz"
 else
    echo "database schema musicbrainz already exists"
 fi
@@ -85,7 +97,7 @@ fi
 find /www/sqls/ -type f | xargs sed -i 's/CREATE TABLE IF NOT EXISTS/CREATE TABLE/g'
 find /www/sqls/ -type f | xargs sed -i 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g'
 find /www/sqls/ -type f | xargs sed -i 's/\\set ON_ERROR_STOP 1/\\unset ON_ERROR_STOP/g'
-if [ -n $PGHOST ]; then
+if [ ! -z "{$PGHOST// }" ]; then
    echo "using environment variables PGHOST=$PGHOST and PGPORT=$PGPORT to run sql initialization statements..."
 else 
    echo "using --link as the target database to run sql initialization statements..."
