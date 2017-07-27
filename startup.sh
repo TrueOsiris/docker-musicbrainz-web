2398a70f242e06824443a722a49511baf5d26d91
Osiris <tim@chaubet.be>
2017-07-27 12:30:28 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -28,7 +28,8 @@ sanitize_sql_file() {
    sed -i 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' $1
    sed -i 's/\\set ON_ERROR_STOP 1/\\unset ON_ERROR_STOP/g' $1
    sed -i 's/\-\-.*$//g' $1
-   sed -i -r -e 's/\n/ /g' $1
+   #sed -i -r -e 's/\n/ /g' $1
+   sed -i ':a;N;$!ba;s/\n/ /g' $1
    sed -i 's/\t/ /g;s/ \+/ /g' $1 
    sed -i -r -e 's/(CREATE|ALTER)/\n\n&/g' $1
 }
