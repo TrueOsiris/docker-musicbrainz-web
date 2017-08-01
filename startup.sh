7839b88440898c2a7d0c198494bbab4d44026896
Osiris <tim@chaubet.be>
2017-08-01 17:14:54 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -121,6 +121,7 @@ sed -i 's/^\#\ \ \ }\,/   },/' /musicbrainz-server/lib/DBDefs.pm
 sed -i 's/^\# sub REPLICATION_TYPE { RT_STANDALONE }/ sub REPLICATION_TYPE { RT_SLAVE }/' /musicbrainz-server/lib/DBDefs.pm
 sed -i 's/^\# sub REPLICATION_ACCESS_TOKEN { "" }/ sub REPLICATION_ACCESS_TOKEN { "'"$BRAINZCODE"'" }/' /musicbrainz-server/lib/DBDefs.pm
 sed -i 's/^sub WEB_SERVER[\ \t]\+{ \"www.musicbrainz.example.com\" }/sub WEB_SERVER { \"'"$WEBURL"'\" }/g' /musicbrainz-server/lib/DBDefs.pm
+sed -i '/MUSICBRAINZ_USE_PROXY/a MUSICBRAINZ_USE_PROXY=1/' /musicbrainz-server/lib/DBDefs.pm
 
 if [ ! -f /www/$(echo $initfile) ]; then
    cd /musicbrainz-server
